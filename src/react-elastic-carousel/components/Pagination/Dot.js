fda7598287d4abc545bac7c70c35b2fda6af69a3
Rocco Martella <martella@petesso.de>
2020-05-07 14:19:41 +0200

    change color from dots

--- src/react-elastic-carousel/components/Pagination/Dot.js
+++ src/react-elastic-carousel/components/Pagination/Dot.js
@@ -4,14 +4,19 @@ import styled from "styled-components";
 import { cssPrefix } from "../../utils/helpers";
 
 const boxShadow = "0 0 1px 2px rgba(0, 0, 0, 0.5)";
-const activeBoxShadow = "0 0 1px 3px rgba(103,58,183,1)";
-const hoveredBoxShadow = "0 0 1px 3px rgba(103,58,183,.5)";
+// const activeBoxShadow = "0 0 1px 3px rgba(103,58,183,1)";
+// const hoveredBoxShadow = "0 0 1px 3px rgba(103,58,183,.5)";
+
+const inActiveDot = "#D8D8D8";
+const activeDot = "#515151";
+const activeBoxShadow = "0 0 1px 3px rgba(81,81,81,1)";
+const hoveredBoxShadow = "0 0 1px 3px rgba(81,81,81,.5)";
 
 const Dot = styled.div`
   transition: all 250ms ease;
   margin: 5px;
   background-color: ${({ active }) =>
-    active ? "rgba(103,58,183,.5)" : "transparent"};
+    active ? `${activeDot}` : `${inActiveDot}`};
   font-size: 1.3em;
   content: "";
   height: 10px;
@@ -21,7 +26,7 @@ const Dot = styled.div`
   &:hover {
     cursor: pointer;
     box-shadow: ${({ active }) =>
-      active ? activeBoxShadow : hoveredBoxShadow};
+    active ? activeBoxShadow : hoveredBoxShadow};
   }
 `;
 
@@ -38,7 +43,7 @@ class DotContainer extends React.Component {
         active={active}
         className={`${cssPrefix("dot")} ${
           active ? cssPrefix("dot_active") : ""
-        }`}
+          }`}
       />
     );
   }
