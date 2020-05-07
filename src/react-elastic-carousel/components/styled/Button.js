c34715c8eab7386db720a2ba0fa82e86cc1d4669
Rocco Martella <martella@petesso.de>
2020-05-07 14:13:50 +0200

    delete rounded

--- src/react-elastic-carousel/components/styled/Button.js
+++ src/react-elastic-carousel/components/styled/Button.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 export default styled.button`
   transition: all 0.3s ease;
   font-size: 1.6em;
-  background-color: rgba(103, 58, 183, 0.1);
+  // background-color: rgba(103, 58, 183, 0.1);
   color: #333;
-  box-shadow: 0 0 2px 0px #333;
-  border-radius: 50%;
+  // box-shadow: 0 0 2px 0px #333;
+  // border-radius: 50%;
   border: none;
   width: 50px;
   height: 50px;
